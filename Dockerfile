720caa0487f487fe8fe25edfcd694d5b3bd00ccd
Aidan Hahn <aidanhahn@datawire.io>
2021-07-20 11:00:29 -0700

    tweaks for debugging

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang
 WORKDIR /
 COPY svc.go svc.go
+COPY openapi.json openapi.json
 RUN go build svc.go
 RUN chmod +x svc
 ENTRYPOINT /svc
--- svc.go
+++ svc.go
@@ -4,54 +4,33 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"io/ioutil"
 	"net/http"
 )
 
 var req int
-var apiSpec string = `
-{
-  "openapi": "3.0.0",
-  "info": {
-    "title": "API Spec API",
-    "version": "1.0.0"
-  },
-  "paths": {
-    "/": {
-      "get": {
-        "operationId": "get this spec",
-        "summary": "Returns this specification",
-        "responses": {
-          "200": {
-            "description": "200 response",
-            "content": {
-              "schema": {
-                type: object
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
+var apiSpec string = "/openapi.json"
 
-func endpoint(w http.ResponseWriter, r *http.Request){
-	reqNum := req
-	req += 1
+func handleRequests(b string) {
+	http.HandleFunc("/endpoint", func (w http.ResponseWriter, r *http.Request) {
+		reqNum := req
+		req += 1
 
-	fmt.Printf("Endpoint hit number %d\n", reqNum)
-	time.Sleep(3 * time.Second)
-	fmt.Printf("Responding to request %d\n", reqNum)
+		fmt.Printf("Endpoint hit number %d\n", reqNum)
+		time.Sleep(3 * time.Second)
+		fmt.Printf("Responding to request %d\n", reqNum)
 
-	fmt.Fprintf(w, apiSpec)
-}
+		fmt.Fprintf(w, b)
+	})
 
-func handleRequests() {
-	http.HandleFunc("/endpoint", endpoint)
 	log.Fatal(http.ListenAndServe(":10001", nil))
 }
 
 func main() {
-	handleRequests()
+	b, err := ioutil.ReadFile(apiSpec)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	handleRequests(string(b))
 }
